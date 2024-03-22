7ac432f39e2b2ab9dc5407b1ae97d763afd91ac6
Thiago Costa Santana <thikimlara@gmail.com>
2024-03-22 13:59:34 -0300

    ataulizacao das pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import List from "./components/List";
-
+import { useCallback } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { List } from './components';
+import { favoritesActions } from './store/favorites';
 
 function App() {
+  const dispatch = useDispatch();
   const [items, setItems] = useState([]);
 
+  const favorites = useSelector(({ favorites }) => favorites);
 
   const loadDetails = (items) => {
-   const promises = items.map((item) => {
+    const promises = items.map((item) => {
       return fetch(item.url).then((response) => response.json())
-   });
-   Promise.all(promises).then((data) => {
-      setItems(data);
-   });
-}
+    });
+    Promise.all(promises)
+      .then((data) => {
+        setItems(data);
+      });
+  }
 
   useEffect(() => {
-   fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-   .then((response) => {
-      return response.json();
-   })
-   .then((data) => {
-     const { results } = data;
-     loadDetails(results);
+    // Inicialização
+    const localData = localStorage.getItem('react-redux');
+    if (localData) {
+        const parsed = JSON.parse(localData);
+        const { favorites } = parsed;
+        dispatch(favoritesActions.init(favorites));
+    }
 
-   })
-   .catch(() => {
-     console.error('error');
-   });
-}, []);
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      .then((response) => {
+          return response.json();
+      })
+      .then((data) => {
+        const { results } = data;
+        loadDetails(results);
+      })
+      .catch(() => {
+        console.error('Error');
+      });
+  }, [dispatch]);
 
-   return (
-      <div>
-         <List items={items} />
-      </div>
-   );
+  const handleAddItem = useCallback((item) => {
+    dispatch(favoritesActions.add(item));
+  }, [dispatch]);
+
+  const handleRemoveItem = useCallback((item) => {
+    dispatch(favoritesActions.remove(item));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <List 
+        favorites={favorites}
+        items={items}
+        onAddItem={handleAddItem}
+        onRemoveItem={handleRemoveItem}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppProvider } from './context/Appcontext';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-    <App />
-    </AppProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
